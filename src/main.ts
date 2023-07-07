ece142c4739f553132a70a79333e875bcad174ce
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-07-06 21:09:28 -0400

    :heavy_plus_sign: Add ant design

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue';
 
 import App from './App.vue'
 import router from './router'
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Antd)
 app.mount('#app')
