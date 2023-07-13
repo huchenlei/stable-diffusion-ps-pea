8d3c634f2736292db6bd33126c4fcec18902e867
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-07-12 21:45:00 -0400

    :art: Use affix instead of custom CSS

--- src/App.vue
+++ src/App.vue
@@ -7,33 +7,20 @@ const route = useRoute();
   <!-- Extra layer of div to bypass grid constraint -->
   <div>
     <header>
-      <a-menu class="navigation" mode="horizontal" :selectedKeys="[route.path]">
-        <a-menu-item key="/">
-          <RouterLink to="/">{{ $t('nav.connection') }}</RouterLink>
-        </a-menu-item>
-        <a-menu-item key="/generation">
-          <RouterLink to="/generation">{{ $t('nav.generation') }}</RouterLink>
-        </a-menu-item>
-        <a-menu-item key="/about">
-          <RouterLink to="/about">{{ $t('nav.about') }}</RouterLink>
-        </a-menu-item>
-      </a-menu>
+      <a-affix :offset-top="0">
+        <a-menu class="navigation" mode="horizontal" :selectedKeys="[route.path]">
+          <a-menu-item key="/">
+            <RouterLink to="/">{{ $t('nav.connection') }}</RouterLink>
+          </a-menu-item>
+          <a-menu-item key="/generation">
+            <RouterLink to="/generation">{{ $t('nav.generation') }}</RouterLink>
+          </a-menu-item>
+          <a-menu-item key="/about">
+            <RouterLink to="/about">{{ $t('nav.about') }}</RouterLink>
+          </a-menu-item>
+        </a-menu>
+      </a-affix>
     </header>
-    <RouterView class="view"/>
+    <RouterView class="view" />
   </div>
 </template>
-
-<style scoped>
-.navigation {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1000;
-  /* ensure it's on top of other elements */
-}
-.view {
-  margin-top: 50px;
-}
-
-</style>
