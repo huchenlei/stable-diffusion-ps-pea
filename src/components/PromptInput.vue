d7cf86321dab9910bdd91b9bde61c11d7c3d629e
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-07-30 23:33:18 -0400

    :bug: Fix number format issue

--- src/components/PromptInput.vue
+++ src/components/PromptInput.vue
@@ -118,7 +118,7 @@ export default {
                 return (num / 1e6).toFixed(1).replace(/\.0$/, '') + 'M';
             }
             if (num >= 1e3) {
-                return (num / 1e6).toFixed(1).replace(/\.0$/, '') + 'K';
+                return (num / 1e3).toFixed(1).replace(/\.0$/, '') + 'K';
             }
             return num.toString();
         }
