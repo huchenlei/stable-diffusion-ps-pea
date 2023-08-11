7bc1d3f6d3da17e461a9b0d2f0773444d0a22e4f
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-08-11 18:43:56 -0400

    :white_check_mark: Add test on applyChange

--- src/components/__tests__/DeepDiff.spec.ts
+++ src/components/__tests__/DeepDiff.spec.ts
@@ -1,7 +1,8 @@
-import { diff } from 'deep-diff';
+import { diff, applyChange, type Diff} from 'deep-diff';
 import { describe, it, expect } from 'vitest'
 import { ApplicationState } from '../../Core';
 import { ControlNetUnit } from '../../ControlNet';
+import _ from 'lodash';
 
 describe('diff', () => {
     it('Should detect value change', () => {
@@ -35,5 +36,49 @@ describe('diff', () => {
                 rhs: true,
             }
         ]);
+    });
+});
+
+describe('applyChange', () => {
+    it('Should apply array change', () => {
+        const newUnit = {
+            "batch_images": "",
+            "control_mode": 2,
+            "enabled": true,
+            "guidance_end": 1,
+            "guidance_start": 0,
+            "input_mode": 0,
+            "low_vram": false,
+            "model": "control_v11p_sd15_inpaint [ebff9138]",
+            "module": "inpaint_only+lama",
+            "output_dir": "",
+            "pixel_perfect": false,
+            "processor_res": 512,
+            "resize_mode": 1,
+            "threshold_a": 64,
+            "threshold_b": 64,
+            "weight": 1,
+            "linkedLayerName": ""
+        };
+
+        const base = new ApplicationState();
+        base.controlnetUnits = [new ControlNetUnit()];
+        const expected = _.cloneDeep(base);
+        expected.controlnetUnits[10] = newUnit as any;
+        
+        const stateChange = {
+            "kind": "A",
+            "path": [
+                "controlnetUnits"
+            ],
+            "index": 10,
+            "item": {
+                "kind": "N",
+                "rhs": newUnit,
+            }
+        } as unknown as Diff<ApplicationState>;
+
+        applyChange(base, undefined, stateChange);
+        expect(base).toEqual(expected);
     });
 });
