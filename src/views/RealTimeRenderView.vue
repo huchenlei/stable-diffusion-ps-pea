1a63b39a8a2def181948d02580d1f0b8276145ae
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-11-24 20:44:39 -0500

    :construction: Add prompt input

--- src/views/RealTimeRenderView.vue
+++ src/views/RealTimeRenderView.vue
@@ -2,20 +2,16 @@
 import { ref, computed } from 'vue';
 import { useAppStateStore } from '@/stores/appStateStore';
 import DiceOutlined from '@/components/svg/DiceOutlined.vue';
+import PromptInput from '@/components/PromptInput.vue';
 
 const appStateStore = useAppStateStore();
 const appState = appStateStore.appState;
 
-// Seed control.
-// - Display the current active seed
-// - A button that reroll the seed
-// - A list of previously used seeds
+
 function rerollSeed() {
   // Generate a 32-bit integer
   appState.commonPayload.seed = Math.floor(Math.random() * Math.pow(2, 32));
 }
-
-// Prompt input
 
 // Send to canvas button
 // Send the rendered image to the canvas (selection)
@@ -23,12 +19,18 @@ function rerollSeed() {
 
 <template>
   <a-space direction="vertical" style="width: 100%;">
+     <!-- Seed control.
+     - Display the current active seed
+     - A button that reroll the seed
+     - A list of previously used seeds -->
     <a-row>
       <a-input-number :addonBefore="$t('gen.seed')" v-model:value="appState.commonPayload.seed"></a-input-number>
       <a-button @click="rerollSeed">
         <DiceOutlined></DiceOutlined>
       </a-button>
     </a-row>
+
+    <PromptInput v-model:payload="appState.commonPayload"></PromptInput>
   </a-space>
 </template>
 
